afb282f0cc6ff0b05055230fba9668647c2eed4c
João Victor Carvalho <149029065+0Carvalh0@users.noreply.github.com>
2024-03-22 11:58:26 -0300

    Atualização navbar menu

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ const btnMenu = document.getElementById("botaomenu");
 const list = document.getElementById("menu");
 
 function abrirmenu() {
-  if (list.style.left == "-500px") {
+  if (list.style.left <= "-500px") {
     list.style.left = "0px";
     btnMenu.classList.replace("fa-bars", "fa-xmark");
   } else {
@@ -12,6 +12,8 @@ function abrirmenu() {
 }
 
 function mudartamanho() {
-  list.style.left = "-500px";
-  btnMenu.classList.replace("fa-xmark", "fa-bars");
+  if (window.innerWidth < "1400px") {
+    list.style.left = "-500px";
+    btnMenu.classList.replace("fa-xmark", "fa-bars");
+  }
 }
